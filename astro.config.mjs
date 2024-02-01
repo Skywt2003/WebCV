73e888bec68d30d510277774f4037987af93e7bf
SkyWT <me@skywt.cn>
2024-02-01 18:08:46 +0800

    ✨ 独立配置文件，添加英文支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "zh",
+    locales: ["en", "zh"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  vite: {
+    plugins: [yaml()],
+  },
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,42 @@
 /// <reference types="astro/client" />
+
+interface SchoolObject {
+  name: string;
+  college?: string;
+  major: string;
+  degree?: string;
+  date: string;
+  majorCourses: string;
+  score: string;
+}
+
+interface ItemObject {
+  title: string;
+  subtitle?: string;
+  note?: string;
+  date?: string;
+}
+
+interface ProjectObject {
+  title: string;
+  type: string;
+  job?: string;
+  date?: string;
+  desc: string;
+  tech: string;
+  items?: string[];
+}
+
+interface SectionObject {
+  title: string;
+  schools?: [SchoolObject];
+  items?: [ItemObject];
+  projects?: [ProjectObject];
+}
+
+interface Metadata {
+  name: string;
+  phone: string;
+  email: string;
+  intention?: string;
+}
