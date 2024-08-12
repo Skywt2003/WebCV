3adc26a178cf0a3e076d0c074aad84c506eed22e
SkyWT <me@skywt.cn>
2024-08-12 22:48:03 +0800

    ✨ 结构更新

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,56 +8,29 @@ declare module "*.yml" {
 
 interface Metadata {
   name: string;
+  id: string;
   phone: string;
   email: string;
   github: string;
   blog: string;
+  wechat: string;
   photo?: string;
 }
 
 interface Section {
   title: string;
-  schools?: [School];
-  internships?: [Internship];
-  projects?: [Project];
-  items?: [Item];
-  bullets?: [string];
-  table?: boolean;
-  icon?: string;
-}
-
-interface School {
-  name: string;
-  college?: string;
-  major: string;
-  degree?: string;
-  date: string;
-  items?: string[];
-}
-
-interface Internship {
-  name: string;
-  department: string;
-  job: string;
-  date: string;
-  techs?: Tech[];
-  items?: string[];
+  projects: Project[];
+  table: Table[];
 }
 
 interface Project {
   title: string;
-  type: string;
-  job?: string;
-  date?: string;
-  desc: string;
-  techs?: Tech[];
-  items?: string[];
-}
-
-interface Item {
-  title: string;
   subtitle?: string;
+  desc?: string;
   date?: string;
+  caption?: string;
+  techs?: Tech[];
+  items?: (string | string[])[];
 }
 
 interface Tech {
@@ -65,12 +38,5 @@ interface Tech {
   name: string;
 }
 
-// interface TableRow {
-//   name: string;
-//   items: string[];
-// }
 interface Table {
-  // headers: string[];
-  // subHeaders: string[];
-  // rows: TableRow[];
 }
